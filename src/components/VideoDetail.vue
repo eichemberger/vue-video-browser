3276d3cd3fbdd9670790a8a5ef195231a630dfa0
German Eichemberger <germaneichemberger@gmail.com>
2022-10-01 20:00:43 -0300

    first commit 🚀

---
+++ src/components/VideoDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-md-8" v-if="video">
+    <div class="ratio ratio-16x9">
+      <iframe
+        class="embed-responsive-item"
+        :src="videoUrl"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoDetail",
+  props: {
+    video: Object,
+  },
+  computed: {
+    videoUrl() {
+      return `https://www.youtube.com/embed/${this.video.id.videoId}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
